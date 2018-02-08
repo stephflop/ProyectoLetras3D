700909cbf463fc593fe649213bd2c24aea8b1377
fany <fanyta_flolop@hotmail.com>
2018-02-07 22:25:32 -0600

    Primer Commit
    Se subió por primera vez el proyecto

---
+++ letra3DymasPlantilla/backend/conocenos.php
@@ -0,0 +1,241 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Letra3Dymas</title>
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="icon" href="images/icono.jpg">
+        
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/DT_bootstrap.css" >
+        <link rel="stylesheet" href="styles/bootstrap-theme.min.css">
+        
+        <link rel="stylesheet" href="../css/font-awesome.min.css">
+        <link rel="stylesheet" href="../css/style_.css">
+        <link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/fonts.css">
+	<link rel="stylesheet" href="../css/cssFancybox/jquery.fancybox.css">
+
+        <script src="../js/jquery-2.2.0.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<script src="../js/jquery.fancybox.js"></script>
+	<script src="../js/animatescroll.js"></script>
+	<script src="../js/jquery.scrollUp.js"></script>
+        <script src="../js/main.js"></script>
+	<script src="../js/DT_bootstrap.js"></script>
+        
+
+        
+        <script src="../js/script.js"></script>
+    
+
+
+
+</head>
+
+<body>
+
+	<div class="container-fluid">
+
+		<!--=====================================
+		CABEZOTE
+		======================================-->
+		
+		<header class="row">
+			
+			<!-- LOGOTIPO -->
+
+			<center><div id="logo" class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					
+				<img src="images/logotipo.png" class="img-responsive">
+
+                            </div>
+                        <i><h3><b>Iluminamos</b> tu mundo</h3></i>
+                        </center>
+
+		</header>
+		
+		<!--====  Fin de CABEZOTE  ====-->
+                
+                <!--=====================================
+		NAVEGACION
+		======================================-->
+			
+		<div class="row" style="padding-top: 30px">
+                    <nav class="navbar navbar-default" role="navigation">
+                              <div class="container-fluid">
+                                <div class="navbar-header">
+                                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-main"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                                  <a class="navbar-brand" href="#"></a> 
+                                </div>
+                                <div class="collapse navbar-collapse" id="navbar-collapse-main">
+
+                                  <ul class="nav navbar-nav navbar-center">
+                                      <li class="active"><a href="../index.php">Inicio</a></li>
+                                      <li><a href="conocenos.php">Conocenos</a></li>
+                                    <li><a href="javascript:void(0);">Servicios</a></li>
+                                    <li><a href="javascript:void(0);">Productos</a></li>
+                                    <li><a href="javascript:void(0);">Galería</a></li>
+                                    <li><a href="javascript:void(0);">Contacto</a></li>
+                                  </ul>
+
+                                </div>
+                              </div>
+                        </nav>
+                </div
+
+		<!--==== FIN NAVEGACION ====-->
+		
+		
+
+		<!--=====================================
+		SLIDE
+		======================================-->
+		
+                <div class="row" style= "background: white;height: 300px;" >
+                   
+                </div>
+                                <div class="row" style= " padding-left: 50px; padding-top: 70px;padding-bottom: 50px" id="top ">  
+                    <div  style="border-left-style: inset;border-width: 5px;  padding-left: 20px" class="col-xs-12 col-sm-6 col-md-8">
+                        <div style="line-height: 1.1; font-size: 42px; font-weight: 300;">
+                            En <b style="color: gray">Letra 3D y M&aacute;s</b> creamos lo que tu puedas imaginar de
+                            <strong>principio a fin.</strong>
+                        </div><br>
+                        <br>
+                        <div style=" font-size: 17px; color: #555;">Si est&aacute; buscando impresos como tarjetas o volantes, vinil, rotulaci&oacute;n de veh&iacute;culos,
+                            anuncios luminosos,
+                            letras en 3D, herrer&iacute;a publicitaria, espectaculares, se&ntilde;al&eacute;tica... y <b>m&aacute;s</b>, 
+                            <b style="color: midnightblue">nosotros somos tu mejor opci&oacute;n.</b>
+                        </div>
+                        <br><br>
+                        <a href="backend/conocenos.php">
+                            <button type="button" style="box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.50);" class="btn btn-primary btn-lg"><b>Te decimos porqu&eacute;</b></button>
+                        </a>
+                        <br><br>
+                    </div>
+                    
+                    <div style= " " id="top " class="col-xs-12 col-sm-6 col-md-4">
+                        <center><img  class="" src="images/general/publ.jpg" width="350px" height="200px" style="box-shadow: 10px 10px 6px #17202A;border-radius:50px 50px 50px 50px"></a><br>
+                    </center>
+                        <br><br>
+                    </div>
+		</div>
+
+		<!--====  Fin de SLIDE  ====-->
+
+
+        
+        <div class="row footer_top" >
+            <?php //require './backend/footer.php';?>
+            <center>
+                <div class="row" style="margin-top: 20px; margin-bottom: 50px; margin-left: 20px;">
+                    <div class="col-sm-3" >
+                        <h4 style="color: #a8c262; font-weight: 800;">Servicios</h4>
+                        <div style="margin-left: 10px">
+                            <span style="line-height: 2">
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink" href="">Servicio 1</a>
+                                </span>
+                                <br/>
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Servicio 2</a>
+                                </span> 
+                                <br/>
+
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Servicio 3</a>
+                                </span>
+                                 <br/>
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Servicio 4</a>
+                                </span>
+                            </span>
+                        </div>
+                        <br>
+                    </div>
+                    <div class="col-sm-3" >
+                        <h4 style="color: #a8c262; font-weight: 800;">Productos</h4>
+                        <div style="margin-left: 10px">
+                            <span style="line-height: 2">
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink" href="">Producto 1</a>
+                                </span>
+                                <br/>
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Producto 2</a>
+                                </span> 
+                                <br/>
+
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Producto 3</a>
+                                </span>
+                                 <br/>
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Producto 4</a>
+                                </span>
+                            </span>
+                        </div> 
+                        <br>
+
+                    </div>
+                    <div class="col-sm-3" >
+                        <h4 style="color: #a8c262; font-weight: 800;">Acerca de nosotros</h4>
+                        <div style="margin-left: 10px">
+                            <span style="line-height: 2">
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink" href="">Letra 3D &#38; M&aacute;s</a>
+                                </span>
+                                <br/>
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Misi&oacute;n</a>
+                                </span> 
+                                <br/>
+
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Visi&oacute;n</a>
+                                </span>
+                                 <br/>
+                                <span class="glyphicon glyphicon-chevron-right" style="font-size: 9px;color:#a8c262;">
+                                    <a class="footerlink"  href="">Valores</a>
+                                </span>
+                            </span>
+                        </div>
+                         <br>
+                    </div>
+                    <div class="col-sm-3" >
+                        <h4 style="color: #a8c262; font-weight: 800;">S&iacute;guenos</h4>
+                        <div style="margin-left: 10px">
+                            <a href="" target="_blank" title="S&iacute;guenos en Facebook"><img alt="" class="pic2" src="/portals/0/Images/facebook.png" style="width: 58px; margin-top: 15px;">
+                                <img src="../images/general/face.png" width="50px" height="50px" style="line-height: 2">
+
+                            </a>
+
+
+                        </div>
+                    </div>
+                </div>
+            </center>
+        </div>
+        <footer class="row skin_footer">
+            <div class="skin_width">
+              <div class="copyright_bar clearafter">
+                <div class="footer_right">
+                    <span id="dnn_dnnCOPYRIGHT_lblCopyright" class="Footer">&#169; 2018 Letra 3D &#38; M&aacute;s</span>
+
+                |
+                  <i>Iluminamos TU mundo</i>
+                </div>
+              </div>
+            </div>
+        </footer>
+               
+
+
+
+</body>
+</html>
+
+
